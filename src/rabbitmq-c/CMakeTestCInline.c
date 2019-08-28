03afd21afd694b825f5baea8494e16fb4e2f9f3a
root <root@debian>
2019-08-28 14:09:14 +0900

    update

--- src/rabbitmq-c/CMakeTestCInline.c
+++
@@ -1,3 +0,0 @@
-/* Inspired by autoconf's c.m4 */
-static inline int static_foo() {return 0;}
-int main(int argc, char *argv[]){return 0;}
--- src/rabbitmq-c/CTestTestfile.cmake
+++
@@ -1,9 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /bxr_ploverpc/src/rabbitmq-c
-# Build directory: /bxr_ploverpc/src/rabbitmq-c
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("librabbitmq")
-subdirs("examples")
-subdirs("tests")
