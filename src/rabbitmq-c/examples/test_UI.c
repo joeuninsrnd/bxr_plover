a434426b94e968713005439ee2bc129372b2a24d
root <root@debian>
2019-09-09 17:11:00 +0900

    UI update

--- src/rabbitmq-c/examples/test_UI.c
+++ src/rabbitmq-c/examples/test_UI.c
@@ -24,6 +24,7 @@ typedef struct Data_storage
 }data_storage;
 
 static data_storage ds;
+static const gchar *path;
 
 /* Compile the regular expression described by "regex_text" into
    "r". */
@@ -178,7 +179,7 @@ int detect_func()
   amqp_connection_state_t conn;
   amqp_bytes_t reply_to_queue;
 
-  char path[4096] = "/home/joeun/testd";
+  //char path[4096] = "/home/joeun/testd";
   scan_dir(path);
 
 
@@ -188,191 +189,191 @@ int detect_func()
   routingkey = "ka";
 
   /*
-         establish a channel that is used to connect RabbitMQ server
+	 establish a channel that is used to connect RabbitMQ server
   */
 
   conn = amqp_new_connection();
 
   socket = amqp_tcp_socket_new(conn);
   if (!socket) {
-        //die("creating TCP socket");
-        printf("creating TCP socket\n");
+	//die("creating TCP socket");
+	printf("creating TCP socket\n");
   }
 
   status = amqp_socket_open(socket, hostname, port);
   if (status) {
-        //die("opening TCP socket");
-        printf("opening TCP socket\n");
+	//die("opening TCP socket");
+	printf("opening TCP socket\n");
   }
 
 /*
   die_on_amqp_error(amqp_login(conn, "/", 0, 131072, 0, AMQP_SASL_METHOD_PLAIN,
-                                                           "guest", "guest"),
-                                        "Logging in");*/
+							   "guest", "guest"),
+					"Logging in");*/
   amqp_login(conn, "/", 0, 131072, 0, AMQP_SASL_METHOD_PLAIN,
-                                                           "guest", "guest");
-
+							   "guest", "guest");
+					
   amqp_channel_open(conn, 1);
-
+  
   //die_on_amqp_error(amqp_get_rpc_reply(conn), "Opening channel");
   amqp_get_rpc_reply(conn);
 
   /*
-         create private reply_to queue
+	 create private reply_to queue
   */
 
   {
-        amqp_queue_declare_ok_t *r = amqp_queue_declare(
-                conn, 1, amqp_empty_bytes, 0, 0, 0, 1, amqp_empty_table);
-
-
-        //die_on_amqp_error(amqp_get_rpc_reply(conn), "Declaring queue");
-        amqp_get_rpc_reply(conn);
-
-        reply_to_queue = amqp_bytes_malloc_dup(r->queue);
-        if (reply_to_queue.bytes == NULL) {
-          fprintf(stderr, "Out of memory while copying queue name");
-          return 1;
-        }
+	amqp_queue_declare_ok_t *r = amqp_queue_declare(
+		conn, 1, amqp_empty_bytes, 0, 0, 0, 1, amqp_empty_table);
+		
+		
+	//die_on_amqp_error(amqp_get_rpc_reply(conn), "Declaring queue");
+	amqp_get_rpc_reply(conn);
+	
+	reply_to_queue = amqp_bytes_malloc_dup(r->queue);
+	if (reply_to_queue.bytes == NULL) {
+	  fprintf(stderr, "Out of memory while copying queue name");
+	  return 1;
+	}
   }
 
   /*
-         send the message
+	 send the message
   */
 
   {
-        /*
-          set properties
-        */
-        amqp_basic_properties_t props;
-        props._flags = AMQP_BASIC_CONTENT_TYPE_FLAG |
-                                   AMQP_BASIC_DELIVERY_MODE_FLAG | AMQP_BASIC_REPLY_TO_FLAG |
-                                   AMQP_BASIC_CORRELATION_ID_FLAG;
-        props.content_type = amqp_cstring_bytes("text/plain");
-        props.delivery_mode = 2; /* persistent delivery mode */
-        props.reply_to = amqp_bytes_malloc_dup(reply_to_queue);
-        if (props.reply_to.bytes == NULL) {
-          fprintf(stderr, "Out of memory while copying queue name");
-          return 1;
-        }
-        props.correlation_id = amqp_cstring_bytes("1");
+	/*
+	  set properties
+	*/
+	amqp_basic_properties_t props;
+	props._flags = AMQP_BASIC_CONTENT_TYPE_FLAG |
+				   AMQP_BASIC_DELIVERY_MODE_FLAG | AMQP_BASIC_REPLY_TO_FLAG |
+				   AMQP_BASIC_CORRELATION_ID_FLAG;
+	props.content_type = amqp_cstring_bytes("text/plain");
+	props.delivery_mode = 2; /* persistent delivery mode */
+	props.reply_to = amqp_bytes_malloc_dup(reply_to_queue);
+	if (props.reply_to.bytes == NULL) {
+	  fprintf(stderr, "Out of memory while copying queue name");
+	  return 1;
+	}
+	props.correlation_id = amqp_cstring_bytes("1");
 
-        /*
-          publish
-        */
+	/*
+	  publish
+	*/
 
-        /*die_on_error(amqp_basic_publish(conn, 1, amqp_cstring_bytes(exchange),
-                                                                        amqp_cstring_bytes(routingkey), 0, 0,
-                                                                        &props, amqp_cstring_bytes(&ds.jumin)), "Publishing"); //구조체 포인터부분
-        */
-        amqp_basic_publish(conn, 1, amqp_cstring_bytes(exchange),
-                                                                                amqp_cstring_bytes(routingkey), 0, 0,
-                                                                                &props, amqp_cstring_bytes(&ds.jumin));
+	/*die_on_error(amqp_basic_publish(conn, 1, amqp_cstring_bytes(exchange),
+									amqp_cstring_bytes(routingkey), 0, 0,
+									&props, amqp_cstring_bytes(&ds.jumin)), "Publishing"); //구조체 포인터부분
+	*/
+	amqp_basic_publish(conn, 1, amqp_cstring_bytes(exchange),
+										amqp_cstring_bytes(routingkey), 0, 0,
+										&props, amqp_cstring_bytes(&ds.jumin));
 
-        amqp_bytes_free(props.reply_to);
+	amqp_bytes_free(props.reply_to);
   }
 
   /*
-        wait an answer
+	wait an answer
   */
 
   {
-        amqp_basic_consume(conn, 1, reply_to_queue, amqp_empty_bytes, 0, 1, 0,
-                                           amqp_empty_table);
-        //die_on_amqp_error(amqp_get_rpc_reply(conn), "Consuming");
-        amqp_bytes_free(reply_to_queue);
+	amqp_basic_consume(conn, 1, reply_to_queue, amqp_empty_bytes, 0, 1, 0,
+					   amqp_empty_table);
+	//die_on_amqp_error(amqp_get_rpc_reply(conn), "Consuming");
+	amqp_bytes_free(reply_to_queue);
 
-        {
-          amqp_frame_t frame;
-          int result;
+	{
+	  amqp_frame_t frame;
+	  int result;
 
-          amqp_basic_deliver_t *d;
-          amqp_basic_properties_t *p;
-          size_t body_target;
-          size_t body_received;
+	  amqp_basic_deliver_t *d;
+	  amqp_basic_properties_t *p;
+	  size_t body_target;
+	  size_t body_received;
 
-          for (;;) {
-                amqp_maybe_release_buffers(conn);
-                result = amqp_simple_wait_frame(conn, &frame);
-                printf("Result: %d\n", result);
-                if (result < 0) {
-                  break;
-                }
+	  for (;;) {
+		amqp_maybe_release_buffers(conn);
+		result = amqp_simple_wait_frame(conn, &frame);
+		printf("Result: %d\n", result);
+		if (result < 0) {
+		  break;
+		}
 
-                printf("Frame type: %u channel: %u\n", frame.frame_type, frame.channel);
-                if (frame.frame_type != AMQP_FRAME_METHOD) {
-                  continue;
-                }
+		printf("Frame type: %u channel: %u\n", frame.frame_type, frame.channel);
+		if (frame.frame_type != AMQP_FRAME_METHOD) {
+		  continue;
+		}
 
-                printf("Method: %s\n", amqp_method_name(frame.payload.method.id));
-                if (frame.payload.method.id != AMQP_BASIC_DELIVER_METHOD) {
-                  continue;
-                }
+		printf("Method: %s\n", amqp_method_name(frame.payload.method.id));
+		if (frame.payload.method.id != AMQP_BASIC_DELIVER_METHOD) {
+		  continue;
+		}
 
-                d = (amqp_basic_deliver_t *)frame.payload.method.decoded;
-                printf("Delivery: %u exchange: %.*s routingkey: %.*s\n",
-                           (unsigned)d->delivery_tag, (int)d->exchange.len,
-                           (char *)d->exchange.bytes, (int)d->routing_key.len,
-                           (char *)d->routing_key.bytes);
+		d = (amqp_basic_deliver_t *)frame.payload.method.decoded;
+		printf("Delivery: %u exchange: %.*s routingkey: %.*s\n",
+			   (unsigned)d->delivery_tag, (int)d->exchange.len,
+			   (char *)d->exchange.bytes, (int)d->routing_key.len,
+			   (char *)d->routing_key.bytes);
 
-                result = amqp_simple_wait_frame(conn, &frame);
-                if (result < 0) {
-                  break;
-                }
+		result = amqp_simple_wait_frame(conn, &frame);
+		if (result < 0) {
+		  break;
+		}
 
-                if (frame.frame_type != AMQP_FRAME_HEADER) {
-                  fprintf(stderr, "Expected header!");
-                  abort();
-                }
-                p = (amqp_basic_properties_t *)frame.payload.properties.decoded;
-                if (p->_flags & AMQP_BASIC_CONTENT_TYPE_FLAG) {
-                  printf("Content-type: %.*s\n", (int)p->content_type.len,
-                                 (char *)p->content_type.bytes);
-                }
-                printf("----\n");
+		if (frame.frame_type != AMQP_FRAME_HEADER) {
+		  fprintf(stderr, "Expected header!");
+		  abort();
+		}
+		p = (amqp_basic_properties_t *)frame.payload.properties.decoded;
+		if (p->_flags & AMQP_BASIC_CONTENT_TYPE_FLAG) {
+		  printf("Content-type: %.*s\n", (int)p->content_type.len,
+				 (char *)p->content_type.bytes);
+		}
+		printf("----\n");
 
-                body_target = (size_t)frame.payload.properties.body_size;
-                body_received = 0;
+		body_target = (size_t)frame.payload.properties.body_size;
+		body_received = 0;
 
-                while (body_received < body_target) {
-                  result = amqp_simple_wait_frame(conn, &frame);
-                  if (result < 0) {
-                        break;
-                  }
+		while (body_received < body_target) {
+		  result = amqp_simple_wait_frame(conn, &frame);
+		  if (result < 0) {
+			break;
+		  }
 
-                  if (frame.frame_type != AMQP_FRAME_BODY) {
-                        fprintf(stderr, "Expected body!");
-                        abort();
-                  }
+		  if (frame.frame_type != AMQP_FRAME_BODY) {
+			fprintf(stderr, "Expected body!");
+			abort();
+		  }
 
-                  body_received += frame.payload.body_fragment.len;
-                  assert(body_received <= body_target);
+		  body_received += frame.payload.body_fragment.len;
+		  assert(body_received <= body_target);
 
-                  /*amqp_dump(frame.payload.body_fragment.bytes,
-                                        frame.payload.body_fragment.len);*/
-                }
+		  /*amqp_dump(frame.payload.body_fragment.bytes,
+					frame.payload.body_fragment.len);*/
+		}
 
-                if (body_received != body_target) {
-                  /* Can only happen when amqp_simple_wait_frame returns <= 0 */
-                  /* We break here to close the connection */
-                  break;
-                }
+		if (body_received != body_target) {
+		  /* Can only happen when amqp_simple_wait_frame returns <= 0 */
+		  /* We break here to close the connection */
+		  break;
+		}
 
-                /* everything was fine, we can quit now because we received the reply */
-                break;
-          }
-        }
+		/* everything was fine, we can quit now because we received the reply */
+		break;
+	  }
+	}
   }
 
   /*
-         closing
+	 closing
   */
 
 /*
   die_on_amqp_error(amqp_channel_close(conn, 1, AMQP_REPLY_SUCCESS),
-                                        "Closing channel");
+					"Closing channel");
   die_on_amqp_error(amqp_connection_close(conn, AMQP_REPLY_SUCCESS),
-                                        "Closing connection");
+					"Closing connection");
   die_on_error(amqp_destroy_connection(conn), "Ending connection");
 */
   amqp_channel_close(conn, 1, AMQP_REPLY_SUCCESS);
@@ -383,32 +384,36 @@ int detect_func()
   return 0;
 }
 
-GtkWidget *g_btn_detect;
-GtkWidget *g_cbt;
-GtkWidget *g_clb;
 
+GtkEntry *entry_detect;
 
 int main(int argc, char *argv[])
 {
     GtkBuilder *gtkBuilder;
     GtkWidget *window;
     gtk_init(&argc, &argv);
-
+    
     gtkBuilder = gtk_builder_new();
     gtk_builder_add_from_file(gtkBuilder, "tglade.glade", NULL);
     window = GTK_WIDGET(gtk_builder_get_object(gtkBuilder, "appwindow1"));
+    entry_detect = GTK_ENTRY(gtk_builder_get_object(gtkBuilder, "entry_detect"));
     gtk_builder_connect_signals(gtkBuilder, NULL);
-
+    
 
     g_object_unref(G_OBJECT(gtkBuilder));
     gtk_widget_show(window);
     gtk_main();
-
+    
     return 0;
+}
+
+void on_entry_detect_activate(GtkEntry *entry_detect)
+{
+	path = gtk_entry_get_text(entry_detect);
+	g_print("선택한 폴더 위치: %s\n", path);
 }
 
 void on_btn_detect_clicked()
 {
     detect_func();
 }
-
