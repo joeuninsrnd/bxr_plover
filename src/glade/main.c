1e9a59e0cba2b9f1714cf66bcc85073af6533c1c
root <root@debian>
2019-09-19 18:09:37 +0900

    UI update

--- src/glade/main.c
+++ src/glade/main.c
@@ -1,23 +1,88 @@
 #include <gtk/gtk.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stdint.h>
+#include <regex.h>
+#include <errno.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <unistd.h>
+#include <amqp.h>
+#include <amqp_tcp_socket.h>
+#include <assert.h>
+#include <dirent.h>
+
+
+
+
+int chk_tf;
+
+GtkWidget *detect_window;
+GtkWidget *setting_window;
+
+GtkWidget *m_detect_btn;
+GtkWidget *m_setting_btn;
+
+void on_m_detect_btn_clicked(GtkButton *m_detect_btn, gpointer *data);
+void on_m_setting_btn_clicked(GtkButton *m_setting_btn, gpointer *data);
+
+//계정이 있는지 확인: TRUE(1)=있다 FALSE(0)=없다
+int chk_user()
+{
+	chk_tf = TRUE;
+	
+	return chk_tf;
+}
+
+void on_m_detect_btn_clicked(GtkButton *m_detect_btn, gpointer *data)
+{
+	gtk_widget_show(detect_window);
+	
+	return;
+}
+
+void on_m_setting_btn_clicked(GtkButton *m_setting_btn, gpointer *data)
+{
+	gtk_widget_show(setting_window);
+	
+	return;
+}
 
 int main(int argc, char *argv[])
 {
-    GtkBuilder      *builder;
-    GtkWidget       *window;
+    GtkBuilder	*builder;
+    GtkWidget		*main_window,
+					*enrollment_window;
 
     gtk_init(&argc, &argv);
 
     builder = gtk_builder_new();
     gtk_builder_add_from_file (builder, "main.glade", NULL);
 
-    window = GTK_WIDGET(gtk_builder_get_object(builder, "window_main"));
+    main_window = GTK_WIDGET(gtk_builder_get_object(builder, "main_window"));
+    enrollment_window = GTK_WIDGET(gtk_builder_get_object(builder, "enrollment_window"));
+    
     gtk_builder_connect_signals(builder, NULL);
-
+    
+    m_detect_btn		= GTK_WIDGET(gtk_builder_get_object(builder, "m_detect_btn"));
+    m_setting_btn	= GTK_WIDGET(gtk_builder_get_object(builder, "m_setting_btn"));
 
     g_object_unref(builder);
 
-    gtk_widget_show(window);
-    gtk_main();
+	chk_user(chk_tf);
+	
+	if(chk_tf == FALSE)
+	{
+		gtk_widget_show(enrollment_window);
+		gtk_main();
+	}
+	
+	if(chk_tf == TRUE)
+	{
+		gtk_widget_show(main_window);
+		gtk_main();
+	}
 
     return 0;
 }
