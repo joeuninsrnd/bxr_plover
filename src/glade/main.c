9c219d17cd396ccdc27873d33828c4d8f9765a42
jun08111 <jun08111@naver.com>
2019-10-31 09:53:23 +0900

    Update main.c

--- src/glade/main.c
+++ src/glade/main.c
@@ -39,14 +39,14 @@ udata_storage uDs;
 
 typedef struct Fdata_storage
 {
-	char fname[100];			// 파일 이름 //
-	uint jcnt;				// 주민번호 개수 //
-	uint dcnt;				// 운전면허 개수 //
-	uint fgcnt;				// 외국인등록번호 개수 //
-	uint pcnt;				// 여권번호 개수 //
-	uint fsize;				// 파일 크기 //
-	char stat[20];				// 파일 상태 //
-	char fpath[300];			// 파일 경로 //
+	char fname[100];		// 파일 이름 //
+	uint jcnt;			// 주민번호 개수 //
+	uint dcnt;			// 운전면허 개수 //
+	uint fgcnt;			// 외국인등록번호 개수 //
+	uint pcnt;			// 여권번호 개수 //
+	uint fsize;			// 파일 크기 //
+	char stat[20];			// 파일 상태 //
+	char fpath[300];		// 파일 경로 //
 
 }fdata_storage;
 
